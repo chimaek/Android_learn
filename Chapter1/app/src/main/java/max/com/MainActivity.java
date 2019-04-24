8d5e00a57ef76395b8e5b1621a7a9d536f0a9338
chimaek <pipiru100@gmail.com>
2019-04-24 12:40:47 +0900

    3.7

--- Chapter1/app/src/main/java/max/com/MainActivity.java
+++ Chapter1/app/src/main/java/max/com/MainActivity.java
@@ -2,14 +2,32 @@ package max.com;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    int clickCount = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(getApplicationContext(),"안녕",Toast.LENGTH_SHORT).show();
+
+        findViewById(R.id.test).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickCount += 1;
+                Toast.makeText(getApplicationContext(),"ClickCount" + clickCount,Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+    int var1 =0;
+    {
+        var1 = 2; // 접근가능
+
+        int var2 = 0;
+    }
+
+//   접근불가 var2 =1;
 }
