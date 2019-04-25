f3d087adfec5f2cc21b074d0c3f398f283aa2106
chimaek <pipiru100@gmail.com>
2019-04-25 09:23:17 +0900

    while

--- Chapter1/app/src/main/java/max/com/MainActivity.java
+++ Chapter1/app/src/main/java/max/com/MainActivity.java
@@ -7,33 +7,34 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     int clickCount = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(getApplicationContext(),"안녕",Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "안녕", Toast.LENGTH_SHORT).show();
 
         findViewById(R.id.test).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 clickCount += 1;
-                if (clickCount % 2 == 0) {
-                    Toast.makeText(getApplicationContext(),"ClickCount" + clickCount,Toast.LENGTH_SHORT).show();
-                }else if(clickCount % 3 == 0){
-                    Toast.makeText(getApplicationContext(),"Hello world!",Toast.LENGTH_SHORT).show();
-                }else{
-                    Toast.makeText(getApplicationContext(),"hello",Toast.LENGTH_SHORT).show();
+                String text = "";
+                int i = 0;
+                while (1 < clickCount) {
+                    text = text + "안녕";
                 }
+                Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
             }
         });
-    }
 
-    int var1 =0;
-    {
-        var1 = 2; // 접근가능
-
-        int var2 = 0;
     }
+}
+//    int var1 =0;
+//    {
+//        var1 = 2; // 접근가능
+//
+//        int var2 = 0;
+//    }
 
 //   접근불가 var2 =1;
-}
+
