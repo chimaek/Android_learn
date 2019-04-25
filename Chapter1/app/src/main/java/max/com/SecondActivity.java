56062c3360e9557da71356508edf28fcd3a1023f
chimaek <pipiru100@gmail.com>
2019-04-25 10:42:53 +0900

    before function

--- Chapter1/app/src/main/java/max/com/SecondActivity.java
+++ Chapter1/app/src/main/java/max/com/SecondActivity.java
@@ -2,12 +2,52 @@ package max.com;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
+
+    int clickcount = 0;
+    int clickcount2 = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Toast.makeText(getApplicationContext(), "프로그래밍을 시작해보자!", Toast.LENGTH_LONG).show();
+
+        findViewById(R.id.test3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickcount += 1;
+
+                if (clickcount % 2 == 0) {
+                    Toast.makeText(getApplicationContext(), "클릭횟수" + clickcount, Toast.LENGTH_LONG).show();
+                } else if (clickcount % 3 == 0) {
+                    Toast.makeText(getApplicationContext(), "Hello World!", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Hello", Toast.LENGTH_LONG).show();
+                }
+            }
+
+        });
+
+        findViewById(R.id.test4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickcount2 += 1;
+
+                if (clickcount2 % 2 == 0) {
+                    Toast.makeText(getApplicationContext(), "클릭횟수" + clickcount2, Toast.LENGTH_LONG).show();
+                } else if (clickcount2 % 3 == 0) {
+                    Toast.makeText(getApplicationContext(), "Hello World!", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Hello", Toast.LENGTH_LONG).show();
+                }
+            }
+
+        });
+
     }
 }
