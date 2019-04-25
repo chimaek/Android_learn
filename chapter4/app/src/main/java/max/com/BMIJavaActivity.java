57b7a61ffeb0370a3f1b14fff702a87a9d13cf23
Chimaek <pipiru100@gmail.com>
2019-04-25 23:13:44 +0900

    make kotlin onclick

---
+++ chapter4/app/src/main/java/max/com/BMIJavaActivity.java
@@ -0,0 +1,15 @@
+package max.com;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BMIJavaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_view_binding);
+
+
+    }
+}
---
+++ chapter4/app/src/main/java/max/com/BMIKOTLINActivity.kt
@@ -0,0 +1,12 @@
+package max.com
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BMIKOTLINActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_view_binding)
+    }
+}
