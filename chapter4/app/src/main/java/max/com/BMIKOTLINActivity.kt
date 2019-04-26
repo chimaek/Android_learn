766042ab0f6f91f388b780c3258c3dfff2371d23
chimaek <pipiru100@gmail.com>
2019-04-26 21:39:08 +0900

    BMI Kotlin

--- chapter4/app/src/main/java/max/com/BMIKOTLINActivity.kt
+++ chapter4/app/src/main/java/max/com/BMIKOTLINActivity.kt
@@ -2,11 +2,23 @@ package max.com
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.layout_view_binding.*
 
 class BMIKOTLINActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_view_binding)
+
+        btnBMI.setOnClickListener {
+            val heightval = height.text.toString().toDouble();
+
+            val weightval = weight.text.toString().toDouble();
+
+            val bmi = weightval / Math.pow(heightval / 100, 2.0);
+
+            resultLabel.text = "키: ${height.text} 체중 : ${weight.text}  BMI : $bmi"
+
+        }
     }
 }
