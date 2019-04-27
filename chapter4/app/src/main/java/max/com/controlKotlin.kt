6e2c8070a0a661f1fd27dece8f6d0014307115dd
chimaek <pipiru100@gmail.com>
2019-04-27 13:14:27 +0900

    control kotlin

--- chapter4/app/src/main/java/max/com/controlKotlin.kt
+++ chapter4/app/src/main/java/max/com/controlKotlin.kt
@@ -2,11 +2,33 @@ package max.com
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.layout_control.*
 
 class controlKotlin : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_control)
+
+
+        button5.setOnClickListener {
+            val number = editText.text.toString().toInt()
+
+            if (number % 2 == 0) {
+                Toast.makeText(applicationContext, "${number} 는 2의배수입니다.", Toast.LENGTH_LONG).show()
+            } else if (number % 3 == 0) {
+                Toast.makeText(applicationContext, "${number} 는 3의배수입니다.", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(applicationContext, "${number}", Toast.LENGTH_LONG).show()
+            }
+            when (number) {
+                4 -> button5.text = "실행 -4"
+                9 -> button5.text = "실행 -9"
+                else -> button5.text = "실행"
+            }
+
+
+        }
     }
 }
