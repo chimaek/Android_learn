9597ab7ad6414bbb90d1f5adc16e90a7f97f50f0
chimaek <pipiru100@gmail.com>
2019-04-27 15:21:10 +0900

    refactor when

--- chapter4/app/src/main/java/max/com/controlKotlin.kt
+++ chapter4/app/src/main/java/max/com/controlKotlin.kt
@@ -13,19 +13,30 @@ class controlKotlin : AppCompatActivity() {
 
 
         button5.setOnClickListener {
-            val number = editText.text.toString().toInt()
 
-            if (number % 2 == 0) {
-                Toast.makeText(applicationContext, "${number} 는 2의배수입니다.", Toast.LENGTH_LONG).show()
-            } else if (number % 3 == 0) {
-                Toast.makeText(applicationContext, "${number} 는 3의배수입니다.", Toast.LENGTH_LONG).show()
-            } else {
-                Toast.makeText(applicationContext, "${number}", Toast.LENGTH_LONG).show()
-            }
-            when (number) {
-                4 -> button5.text = "실행 -4"
-                9 -> button5.text = "실행 -9"
-                else -> button5.text = "실행"
+            try {
+                val number = editText.text.toString().toInt()
+
+                when {
+                    number % 2 == 0 -> Toast.makeText(
+                        applicationContext,
+                        "${number} 는 2의배수입니다.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    number % 3 == 0 -> Toast.makeText(
+                        applicationContext,
+                        "${number} 는 3의배수입니다.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    else -> Toast.makeText(applicationContext, "${number}", Toast.LENGTH_LONG).show()
+                }
+                when (number) {
+                    in 1..4 -> button5.text = "실행 -4"
+                    9, 18 -> button5.text = "실행 -9"
+                    else -> button5.text = "실행"
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
             }
 
 
