87aae0f8c99253189813f05b3e4b1fbdac558864
Seungju Kim <pipiru100@gmail.com>
2019-04-28 18:26:17 +0900

    Absolute Activity

---
+++ chapter5/app/src/main/java/max/com/AbsoluteActivity.kt
@@ -0,0 +1,12 @@
+package max.com
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AbsoluteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_absolute)
+    }
+}
