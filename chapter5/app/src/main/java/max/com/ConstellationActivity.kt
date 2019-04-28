5b4e12b4f50278c2b79d397cf9d2c1ee49a2ea76
Seungju Kim <pipiru100@gmail.com>
2019-04-28 17:38:27 +0900

    make activities

---
+++ chapter5/app/src/main/java/max/com/ConstellationActivity.kt
@@ -0,0 +1,12 @@
+package max.com
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ConstellationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constellation)
+    }
+}
---
+++ chapter5/app/src/main/java/max/com/NameActvity.kt
@@ -0,0 +1,12 @@
+package max.com
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class NameActvity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_name_actvity)
+    }
+}
